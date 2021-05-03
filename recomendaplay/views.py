899e79ca8761f27b5bd31db7d3f321beb23b5e94
Junior <mrivaldojr@gmail.com>
2021-05-03 00:10:50 -0300

    Deal with Json Response

--- recomendaplay/views.py
+++ recomendaplay/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
 import requests
 from .services import spotify_login_url, get_access_token, get_recent_musics
 
@@ -11,5 +12,14 @@ def login(request):
 
 def home(request):
     r = get_access_token(request.GET.get('code', ''))
-    print(get_recent_musics(r.json()['access_token']).text)
-    return render(request, 'home.html')
+    #print(get_recent_musics(r.json()['access_token']).text)
+    json_response = get_recent_musics(r.json()['access_token'])
+    print(get_music_list(json_response))
+    return render(request, 'home.html', {'music_list':get_music_list(json_response)})
+
+def get_music_list(json_response):
+    musics = json_response.json()
+    music_list = []
+    for i in range(len(musics['items'])):
+        music_list.append(musics['items'][i])
+    return music_list
