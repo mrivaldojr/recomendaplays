896a35f824bd909922ea24217f9ebe55202d511e
Junior <mrivaldojr@gmail.com>
2021-05-02 22:05:49 -0300

    Create Service Py file and move request logic

--- recomendaplay/views.py
+++ recomendaplay/views.py
@@ -1,50 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-import requests, base64
-import environ
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+import requests
+from .services import spotify_login_url, get_access_token, get_recent_musics
 
 def index(request):
     return render(request, 'sign_in.html', {})
 
 def login(request):
-    client_id=env("CLIENT_ID")
-    redirect_url = "http://localhost:8000/home"
-    url = "https://accounts.spotify.com/authorize?client_id="+client_id+"&response_type=code&redirect_uri="+redirect_url+"&scope=user-read-private%20user-read-email%20user-read-recently-played&state=34fFs29kd09"
-    return redirect(url)
+    return redirect(spotify_login_url())
 
 def home(request):
     r = get_access_token(request.GET.get('code', ''))
-    print(r.status_code)
-    print(r.json()['access_token'])
-    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-    print(env("CLIENT"))
-    print('------------------------------------------------------------------------')
+    #print(r.status_code)
+    #print(r.json()['access_token'])
     print(get_recent_musics(r.json()['access_token']).text)
     return render(request, 'home.html')
-
-def get_access_token(code):
-    url = 'https://accounts.spotify.com/api/token'
-    payload = {'grant_type': 'authorization_code', 
-        'code': code, 
-        'redirect_uri':'http://localhost:8000/home'
-        }
-    headers = {'Authorization': 'Basic '+get_base64_id()}
-    return requests.post(url, data=payload, headers=headers)
-
-def get_base64_id():
-    client_id=env("CLIENT_ID")
-    client_secret = env("CLIENT_SECRET")
-    client_key = client_id+":"+client_secret
-    key_bytes = client_key.encode('ascii')
-    base64_bytes = base64.b64encode(key_bytes)
-    return base64_bytes.decode('ascii')
-
-def get_recent_musics(token):
-    url = 'https://api.spotify.com/v1/me/player/recently-played'
-    headers = {'Authorization': 'Bearer '+token}
-    return requests.get(url, headers=headers)
